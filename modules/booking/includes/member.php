3962995248bfac6cf817a6920cd975a07801dfcf
David ALEXANDRE <david.alexandre@w6d.io>
2025-01-19 11:42:33 +0100

    perf: add trace in member package

--- modules/booking/includes/member.php
+++ modules/booking/includes/member.php
@@ -56,36 +56,92 @@ class Member
 	}
 
 	/**
+	 * Creates a new member record
 	 * @throws Exception|PDOException
+	 * @return string
 	 */
 	public function create(): string
 	{
-		$post = array_merge($_GET, $_POST);
-		$password = generatePassword(8);
-		$sql = "INSERT INTO `tbl_membre` ( `id_membre`, `status`, `date`, `email`, `password`, `reseau_id`, `nom`, `prenom`, `code_postal`, `ville`, `pays`, `tel_fixe`, `tel_port`, `tva`, `url`, `afficher` ) VALUES (NULL, :status, :date, :email, :password, :reseau, :nom, :prenom, :code_postal, :ville, :pays, :tel_fixe, :tel_port, :tva, :url, :afficher)";
-		$req = $this->conn->prepare($sql);
-		$this->member = [
-			'status' => MemberStatus::CUSTOMER,
-			'date' => date('Y-m-d'),
-			'email' => strtolower($post['email']),
-			'password' => strrev(md5($password)),
-			'reseau' => 0, // Reseau
-			'nom' => ucwords($post['nom']),
-			'prenom' => ucwords($post['prenom']),
-			'code_postal' => $post['code_postal'],
-			'ville' => ucwords($post['ville']),
-			'pays' => $post['pays'],
-			'tel_fixe' => NULL,
-			'tel_port' => $post['tel_port'],
-			'tva' => '',
-			'url' => slug($post['prenom'] . ' ' . $post['nom'] . ' ' . $post['ville']),
-			'afficher' => 1
-		];
-		if (!$req->execute($this->member)) {
-			Logger::error("member.create", ['errorInfo' => $this->conn->errorInfo()]);
-			throw new Exception("failed to create member");
+		try {
+			$post = array_merge($_GET, $_POST);
+
+			// Log the incoming request data
+			Logger::info("member.create.request", [
+				'data' => array_diff_key($post, array_flip(['password'])) // Log request data excluding sensitive fields
+			]);
+
+			$password = generatePassword(8);
+			$sql = "INSERT INTO `tbl_membre` ( `id_membre`, `status`, `date`, `email`, `password`, `reseau_id`, `nom`, `prenom`, `code_postal`, `ville`, `pays`, `tel_fixe`, `tel_port`, `tva`, `url`, `afficher` ) VALUES (NULL, :status, :date, :email, :password, :reseau, :nom, :prenom, :code_postal, :ville, :pays, :tel_fixe, :tel_port, :tva, :url, :afficher)";
+
+			$req = $this->conn->prepare($sql);
+			if (!$req) {
+				Logger::error("member.create.prepare", [
+					'errorInfo' => $this->conn->errorInfo()
+				]);
+				throw new Exception("Failed to prepare member creation statement");
+			}
+
+			$this->member = [
+				'status' => MemberStatus::CUSTOMER,
+				'date' => date('Y-m-d'),
+				'email' => strtolower($post['email']),
+				'password' => strrev(md5($password)),
+				'reseau' => 0, // Reseau
+				'nom' => ucwords($post['nom']),
+				'prenom' => ucwords($post['prenom']),
+				'code_postal' => $post['code_postal'],
+				'ville' => ucwords($post['ville']),
+				'pays' => $post['pays'],
+				'tel_fixe' => NULL,
+				'tel_port' => $post['tel_port'],
+				'tva' => '',
+				'url' => slug($post['prenom'] . ' ' . $post['nom'] . ' ' . $post['ville']),
+				'afficher' => 1
+			];
+
+			// Validate required fields
+			$requiredFields = ['email', 'nom', 'prenom', 'code_postal'];
+			foreach ($requiredFields as $field) {
+				if (empty($this->member[$field])) {
+					Logger::error("member.create.validation", [
+						'missing_field' => $field,
+						'data' => array_diff_key($this->member, array_flip(['password']))
+					]);
+					throw new Exception("Required field missing: $field");
+				}
+			}
+
+			if (!$req->execute($this->member)) {
+				Logger::error("member.create.execute", [
+					'errorInfo' => $this->conn->errorInfo(),
+					'data' => array_diff_key($this->member, array_flip(['password']))
+				]);
+				throw new Exception("Failed to create member");
+			}
+
+			$memberId = $this->conn->lastInsertId();
+
+			// Log successful creation
+			Logger::info("member.create.success", [
+				'member_id' => $memberId,
+				'email' => $this->member['email']
+			]);
+
+			return $memberId;
+
+		} catch (PDOException $e) {
+			Logger::error("member.create.database", [
+				'error' => $e->getMessage(),
+				'data' => array_diff_key($this->member ?? [], array_flip(['password']))
+			]);
+			throw $e;
+		} catch (Exception $e) {
+			Logger::error("member.create.general", [
+				'error' => $e->getMessage(),
+				'data' => array_diff_key($this->member ?? [], array_flip(['password']))
+			]);
+			throw $e;
 		}
-		return $this->conn->lastInsertId();
 	}
 
 	/**
